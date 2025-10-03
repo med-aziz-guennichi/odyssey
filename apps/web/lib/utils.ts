647ce2b8f51585c4a644d8e4ebe82b574d81f608
Aziz Guennichi <medazizguennichi@gmail.com>
2025-10-03 20:46:16 +0100

    Remove outdated documentation files: deployment, getting started, index, CNAME, UI components

--- apps/web/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-/* ODYSSEY-FileCopyrightText: 2025-present Aziz */
-/* ODYSSEY-License-Identifier: MIT */
-
-// Re-export utils from the UI package
-export * from "@repo/ui/lib/utils";
-
--- apps/web/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/* ODYSSEY-FileCopyrightText: 2025-present Aziz */
-/* ODYSSEY-License-Identifier: MIT */
-
-export default {
-  plugins: {
-    "@tailwindcss/postcss": {},
-    autoprefixer: {},
-  },
-};
